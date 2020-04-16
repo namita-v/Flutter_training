ca1c299debd2a733dab4f9ab1910756ac62f475b
Namita Vinod <namita.v@inapp.com>
2020-04-16 17:39:53 +0530

    Stateful Widgets

--- flutter_app/lib/ui/home.dart
+++ flutter_app/lib/ui/home.dart
@@ -1,4 +1,125 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+class Quotes extends StatefulWidget {
+  @override
+  _QuotesState createState() => _QuotesState();
+}
+
+class _QuotesState extends State<Quotes> {
+
+  int _index=0;
+  List quotes = [
+  "Stay Hungry. Stay Foolish. -Steve Jobs",
+  "Good Artists Copy, Great Artists Steal. -Pablo Picasso",
+  "Argue with idiots, and you become an idiot. -Paul Graham",
+  "Be yourself; everyone else is already taken. -Oscar Wilde",
+  "Simplicity is the ultimate sophistication. -Leonardo Da Vinci"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(quotes[_index%quotes.length]),
+
+            FlatButton.icon(
+                onPressed: _showQuote,
+                icon: Icon(Icons.wb_sunny),
+                label: Text("Inspire Me"))
+          ],
+        )
+      ),
+    );
+  }
+
+  _showQuote() {
+    //increment counter by 1
+    setState(() {
+      _index++;
+    });
+
+  }
+}
+
+class BizCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BizCard"),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent.shade100,
+      ),
+      backgroundColor: Colors.black,
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.topCenter ,
+          children: <Widget>[
+            _getCard(),
+            _getAvatar()
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle),title: Text("Account")),
+            BottomNavigationBarItem(icon: Icon(Icons.ac_unit),title: Text("Ac Unit")),
+            BottomNavigationBarItem(icon: Icon(Icons.add_comment),title: Text("Comment")),
+          ],
+          onTap: (int index) => debugPrint("Tapped item: $index"),
+          backgroundColor: Colors.redAccent.shade100,
+      ),
+    );
+  }
+
+  Container _getCard() {
+    return Container(
+      width:350,
+      height: 200,
+      margin: EdgeInsets.all(50.0),
+      decoration: BoxDecoration(
+        color: Colors.redAccent.shade100,
+        borderRadius: BorderRadius.circular(20.0)
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text("Namita Vinod",
+          style: TextStyle(fontSize: 21.0,
+          color: Colors.indigo,
+          fontWeight: FontWeight.w500)),
+          Text("jfbruhui@gkjrh.com"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(Icons.person_outline),
+              Text("T: @kjfejkker")
+            ],
+          )
+        ],
+      ) ,
+    );
+  }
+
+  Container _getAvatar() {
+    return Container(
+      width:100,
+      height:100,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.all(Radius.circular(50.0)),
+        border: Border.all(color: Colors.white,width: 1.2),
+        image: DecorationImage(image: NetworkImage("https://picsum.photos/300/300"),
+              fit: BoxFit.cover)
+      ),
+    );
+  }
+}
+
+
 class ScaffoldExample extends StatelessWidget {
   tapButton(){
     debugPrint("Tapped Button");
@@ -17,6 +138,15 @@ class ScaffoldExample extends StatelessWidget {
               onPressed: tapButton )
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.lightGreenAccent.shade700,
+          child: Icon(Icons.android),
+          onPressed: () => debugPrint("Developer Settings Tapped")),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.account_circle),title: Text("Account")),
+        BottomNavigationBarItem(icon: Icon(Icons.ac_unit),title: Text("Ac Unit")),
+        BottomNavigationBarItem(icon: Icon(Icons.add_comment),title: Text("Comment")),
+      ],onTap: (int index) => debugPrint("Tapped item: $index"),) ,
       backgroundColor: Colors.pinkAccent.shade100,
       body: Container(
         alignment: Alignment.center,
